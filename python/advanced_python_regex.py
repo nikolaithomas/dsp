9166b79ad694349b4c4ee03dd0fffc6e16caaa33
Nicholas Thomas <thnic@sas.upenn.edu>
2016-09-17 16:58:33 -0700

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,21 @@
-PLACE YOUR CODE HERE
+
+
+#Q1. Find how many different degrees there are, and their frequencies: Ex: PhD, ScD, MD, MPH, BSEd, MS, JD, etc.
+
+
+#Q2. Find how many different titles there are, and their frequencies: Ex: Assistant Professor, Professor
+import numpy as np
+import pandas as pd
+from pandas import Series,DataFrame
+
+data_prof = pd.read_clipboard()
+data_prof
+my_list = data_prof["email"].tolist()
+my_list
+
+#Q3. Search for email addresses and put them in a list. Print the list of email addresses.
+
+
+
+#Q4. Find how many different email domains there are (Ex: mail.med.upenn.edu, upenn.edu, email.chop.edu, etc.). Print the list of unique email domains.
+
