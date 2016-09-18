d18bd2abb7dbad0394ca7968a764202cc7b500dc
Nicholas Thomas <thnic@sas.upenn.edu>
2016-09-17 21:52:24 -0700

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,14 @@
-PLACE YOUR CODE HERE
+import numpy as np
+import pandas as pd
+from pandas import Series,DataFrame
+import csv
+url = 'https://raw.githubusercontent.com/nikolaithomas/dsp/master/python/faculty.csv'
+df = pd.read_csv(url, index_col=None)
+df.columns = [x.strip().replace(' ', '_') for x in df.columns]
+
+
+t = df['email'].tolist()
+t
+thecsv = csv.writer(open("emails.csv", 'wb'))
+for row in t:
+    thecsv.writerow([element])
