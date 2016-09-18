2e3007a12881c7e27ab7467837ddcb4d4b0ccbc9
Nicholas Thomas <thnic@sas.upenn.edu>
2016-09-18 15:45:37 -0700

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,26 @@
-PLACE YOUR CODE HERE
+import numpy as np
+import pandas as pd
+from pandas import Series,DataFrame
+import itertools
+
+url = 'https://raw.githubusercontent.com/nikolaithomas/dsp/master/python/faculty.csv'
+df = pd.read_csv(url, index_col=None)
+df.columns = [x.strip().replace(' ', '_') for x in df.columns]
+
+def Cleandata_degree(df):
+    df['name']= df['name'].str.replace('.','') 
+    df['name']= df['name'].str.split(' ')
+Cleandata_degree(df)
+def new_list(list1, list2):
+    for x in list1:
+        list2.append(x[len(x)-1])
+    
+my_list = df['name'].tolist()
+lastname = []
+new_list(my_list, lastname)
+
+df1 = DataFrame({'lastname': lastname})
+
+df2 = df.join(df1)
+
+df2
