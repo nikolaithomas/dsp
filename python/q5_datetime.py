ca21e2c88a9f644bcadfd221a51e6f85a54637eb
Nicholas Thomas <thnic@sas.upenn.edu>
2016-09-18 09:58:41 -0700

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -2,12 +2,29 @@
 
 ####a) 
 date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+date_stop = '07-28-2015'  
+
+from datetime import datetime
+
+date_start = datetime.strptime('12312013', '%m%d%Y')
+date_stop = datetime.strptime('05282015', '%m%d%Y')
+date_stop - date_start
 
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
 
+from datetime import datetime
+
+date_start = datetime.strptime('01-02-2013', '%m-%d-%Y')
+date_stop = datetime.strptime('07-28-2015', '%m-%d-%Y')
+date_stop - date_start
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+
+from datetime import datetime
+
+date_start = datetime.strptime('15-Jan-1994', '%d-%b-%Y')
+date_stop = datetime.strptime('14-Jul-2015', '%d-%b-%Y')
+date_stop - date_start
