ff55427af1613d658a0f85133e1fd7055cdb6965
Nicholas Thomas <thnic@sas.upenn.edu>
2016-09-17 20:34:17 -0700

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -63,6 +63,51 @@ df3['degree'].value_counts()
 
 #Q2. Find how many different titles there are, and their frequencies: Ex: Assistant Professor, Professor
 
+import numpy as np
+import pandas as pd
+from pandas import Series,DataFrame
+
+url = 'https://raw.githubusercontent.com/nikolaithomas/dsp/master/python/faculty.csv'
+df = pd.read_csv(url, index_col=None)
+df.columns = [x.strip().replace(' ', '_') for x in df.columns]
+
+def Cleandata_degree(df):
+    na_vals = "0"
+    na_vals2 = ''
+    df.degree.replace(na_vals, np.nan, inplace=True)
+    df.degree.replace(na_vals2, np.nan, inplace=True)
+    df.set_value(24, 'title', 'Assistant Professor of Biostatistics')
+    df['degree']= df['degree'].str.replace('.','') 
+    df.drop_duplicates(['degree'], keep='last')
+  
+    
+Cleandata_degree(df)
+array1 = df.title.unique()
+len(array1)
+
+Part II
+import numpy as np
+import pandas as pd
+from pandas import Series,DataFrame
+
+url = 'https://raw.githubusercontent.com/nikolaithomas/dsp/master/python/faculty.csv'
+df = pd.read_csv(url, index_col=None)
+df.columns = [x.strip().replace(' ', '_') for x in df.columns]
+
+def Cleandata_degree(df):
+    na_vals = "0"
+    na_vals2 = ''
+    df.degree.replace(na_vals, np.nan, inplace=True)
+    df.degree.replace(na_vals2, np.nan, inplace=True)
+    df.set_value(24, 'title', 'Assistant Professor of Biostatistics')
+    df['degree']= df['degree'].str.replace('.','') 
+    df.drop_duplicates(['degree'], keep='last')
+  
+    
+Cleandata_degree(df)
+array1 = df.title.unique()
+df['title'].value_counts()
+
 
 #Q3. Search for email addresses and put them in a list. Print the list of email addresses.
 
@@ -70,10 +115,14 @@ import numpy as np
 import pandas as pd
 from pandas import Series,DataFrame
 
-data_prof = pd.read_clipboard()
-data_prof
-my_list = data_prof["email"].tolist()
+url = 'https://raw.githubusercontent.com/nikolaithomas/dsp/master/python/faculty.csv'
+df = pd.read_csv(url, index_col=None)
+df.columns = [x.strip().replace(' ', '_') for x in df.columns]
+
+my_list = df['email'].tolist()
 my_list
+
+
 
 #Q4. Find how many different email domains there are (Ex: mail.med.upenn.edu, upenn.edu, email.chop.edu, etc.). Print the list of unique email domains.
 
