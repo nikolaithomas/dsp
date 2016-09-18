b34188950246d03af477873d40e24290ed552449
Nicholas Thomas <thnic@sas.upenn.edu>
2016-09-17 20:12:09 -0700

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,9 +1,71 @@
 
 
 #Q1. Find how many different degrees there are, and their frequencies: Ex: PhD, ScD, MD, MPH, BSEd, MS, JD, etc.
+import numpy as np
+import pandas as pd
+from pandas import Series,DataFrame
 
+url = 'https://raw.githubusercontent.com/nikolaithomas/dsp/master/python/faculty.csv'
+df = pd.read_csv(url, index_col=None)
+df.columns = [x.strip().replace(' ', '_') for x in df.columns]
+
+def Cleandata_degree(df):
+    na_vals = "0"
+    na_vals2 = ''
+    df.degree.replace(na_vals, np.nan, inplace=True)
+    df.degree.replace(na_vals2, np.nan, inplace=True)
+    df['degree']= df['degree'].str.replace('.','') 
+    df.drop_duplicates(['degree'], keep='last')
+  
+    
+Cleandata_degree(df)
+
+s = df['degree'].str.split(' ').apply(Series, 1).stack()  
+s.index = s.index.droplevel(-1)
+s.name = 'degree'
+del df['degree']
+df2 = df.join(s)
+
+Cleandata_degree(df2)
+df3 = df2.dropna()
+array1 = df3.degree.unique()
+len(array1)
+
+PART II
+import numpy as np
+import pandas as pd
+from pandas import Series,DataFrame
+
+url = 'https://raw.githubusercontent.com/nikolaithomas/dsp/master/python/faculty.csv'
+df = pd.read_csv(url, index_col=None)
+df.columns = [x.strip().replace(' ', '_') for x in df.columns]
+
+def Cleandata_degree(df):
+    na_vals = "0"
+    na_vals2 = ''
+    df.degree.replace(na_vals, np.nan, inplace=True)
+    df.degree.replace(na_vals2, np.nan, inplace=True)
+    df['degree']= df['degree'].str.replace('.','') 
+    df.drop_duplicates(['degree'], keep='last')
+  
+    
+Cleandata_degree(df)
+
+s = df['degree'].str.split(' ').apply(Series, 1).stack()  
+s.index = s.index.droplevel(-1)
+s.name = 'degree'
+del df['degree']
+df2 = df.join(s)
+
+Cleandata_degree(df2)
+df3 = df2.dropna()
+df3['degree'].value_counts()
 
 #Q2. Find how many different titles there are, and their frequencies: Ex: Assistant Professor, Professor
+
+
+#Q3. Search for email addresses and put them in a list. Print the list of email addresses.
+
 import numpy as np
 import pandas as pd
 from pandas import Series,DataFrame
@@ -12,10 +74,6 @@ data_prof = pd.read_clipboard()
 data_prof
 my_list = data_prof["email"].tolist()
 my_list
-
-#Q3. Search for email addresses and put them in a list. Print the list of email addresses.
-
-
 
 #Q4. Find how many different email domains there are (Ex: mail.med.upenn.edu, upenn.edu, email.chop.edu, etc.). Print the list of unique email domains.
 
