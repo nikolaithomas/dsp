e68a546a90efc1f980229e54d0b156503234156b
Nicholas Thomas <thnic@sas.upenn.edu>
2016-09-18 22:46:20 -0700

    Update q7_lists.py

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -67,12 +67,14 @@ def linear_merge(list1, list2):
     merged list of all the elements in sorted order. You may modify
     the passed in lists. Ideally, the solution should work in "linear"
     time, making a single pass of both lists.
+def linear_merge(list1, list2):
+ combined  = []
+  while len(list1) and len(list2):
+    if list1[0] < list2[0]:
+      combined.append(list1.pop(0))
+    else:
+      combined.append(list2.pop(0))
 
-    >>> linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc'])
-    ['aa', 'bb', 'cc', 'xx', 'zz']
-    >>> linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz'])
-    ['aa', 'bb', 'cc', 'xx', 'zz']
-    >>> linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
-    ['aa', 'aa', 'aa', 'bb', 'bb']
-    """
-    raise NotImplementedError
+  combined.extend(list1)
+ combined.extend(list2)
+  return combined
